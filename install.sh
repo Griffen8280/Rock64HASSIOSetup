0d80c47a68b16d15f732333838267f6383a357e8
Griffen8280 <42878642+Griffen8280@users.noreply.github.com>
2020-09-29 18:08:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,27 @@ SUDO=''
 if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
-$SUDO apt update && $SUDO apt install python27
+$SUDO apt update && $SUDO apt install python-smbus -y
+
+echo "Should now be installed, now checking revision"
+revision=`python -c "import R64.GPIO as GPIO; print GPIO.RPI_REVISION"`
+
+if [ $revision = "1" ]
+then
+echo "I2C Pins detected as 0"
+cp installConfigs/i2c_lib_0.py ./i2c_lib.py
+else
+echo "I2C Pins detected as 1"
+cp installConfigs/i2c_lib_1.py ./i2c_lib.py
+fi
+echo "I2C Library setup for this revision of Raspberry Pi, if you change revision a modification will be required to i2c_lib.py"
+echo "Now overwriting modules & blacklist. This will enable i2c Pins"
+cp installConfigs/modules /etc/
+cp installConfigs/raspi-blacklist.conf /etc/modprobe.d/
+printf "dtparam=i2c_arm=1\n" >> /boot/config.txt
+
+
+echo "Should be now all finished. Please press any key to now reboot. After rebooting run"
+echo "'sudo python demo_lcd.py' from this directory"
+read -n1 -s
+$SUDO reboot
