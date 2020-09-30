0060a4cf01f5bc9ee1265b98fa1120773daaa7be
Griffen8280 <42878642+Griffen8280@users.noreply.github.com>
2020-09-30 08:53:18 -0400

    Update display_IP.py

--- display_IP.py
+++ display_IP.py
@@ -9,12 +9,13 @@ import subprocess
 #designate the display variable and get the IP of the machine
 display = lcddriver.lcd()
 IP = subprocess.check_output(["hostname","-I"]).split()[0]
+PiStat = subprocess.check_output(["pihole","status"]).split()[2]
 
 #attempt to display the info
 try:
     print("Writing to display")
     while True:
-        display.lcd_display_string("PiHole is Running", 1)
+        display.lcd_display_string("PiHole is " str(PiStat), 1)
         display.lcd_display_string(str(IP), 2)
         # Program then loops with no delay (Can be added with a time.sleep)
 except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
