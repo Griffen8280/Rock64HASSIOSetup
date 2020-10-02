64bb45a76a5f295b77253d0840b61704e8e7b5c0
Griffen8280 <42878642+Griffen8280@users.noreply.github.com>
2020-10-02 17:29:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,9 @@ then
     fi
 fi
 
-$SUDO curl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh | bash -s -m raspberrypi3-64
+$SUDO curl -sL https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh > hassioinstaller.sh
+chmod +x hassioinstaller.sh
+$SUDO hassioinstaller.sh -m raspberrypi3-64
 echo "All done with docker container pull"
 echo "Allowing initial startup to complete"
 sleep 10s
