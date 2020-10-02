b19bc4a49c030c134b302c9e1d28b7238e5dbc2e
Griffen8280 <42878642+Griffen8280@users.noreply.github.com>
2020-10-02 09:00:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,9 +20,12 @@ chmod +x *.py
 cd GPIO
 chmod +x *.py
 cd ../..
+
+#Setup a check to see if python2 and python3 are installed.
 $SUDO cp -R R64 /usr/local/lib/python2.7/dist-packages/
 echo "Rock64 GPIO should now be installed"
 
+#Setup a check to see if docker is already installed then skip setup if not needed
 #Setup the docker subsystem and install Home Assistant/Hass.io Supervisor
 $SUDO apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common network-manager -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | $SUDO apt-key add -
