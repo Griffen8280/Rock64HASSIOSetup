d25e758b03201cfa6a3e4d486872b8882fe06e56
Chris Pence <pencechris1@gmail.com>
2020-10-18 22:01:39 -0400

    finally got this working

--- removebashwelcometweak.sh
+++ removebashwelcometweak.sh
@@ -1,5 +1,5 @@
 # Simple bit of code to remove the bashwelcometweak
 #!/bin/bash
 
-exec su -l $(who | cut -d' ' -f1)
-sed -i '/ROCK64 PROFILE START/,/ROCK64 PROFILE END/d' "$HOME/.bashrc"
+user=$(who | cut -d' ' -f1)
+sed -i '/ROCK64 PROFILE START/,/ROCK64 PROFILE END/d' "/home/$user/.bashrc"
