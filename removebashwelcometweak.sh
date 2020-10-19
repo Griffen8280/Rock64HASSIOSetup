fc91a9abf19754a22217b1ef5aa0718d42ba8550
Chris Pence <pencechris1@gmail.com>
2020-10-18 21:42:40 -0400

    fixed variables

--- removebashwelcometweak.sh
+++ removebashwelcometweak.sh
@@ -1,4 +1,4 @@
 # Simple bit of code to remove the bashwelcometweak
 
 
-sed -i '/ROCK64 PROFILE START/,/ROCK64 PROFILE END/d' "$HOME/.bashrc"
+su -c "sed -i '/ROCK64 PROFILE START/,/ROCK64 PROFILE END/d' '$HOME/.bashrc'" -s /bin/sh $(who | cut -d' ' -f1)
