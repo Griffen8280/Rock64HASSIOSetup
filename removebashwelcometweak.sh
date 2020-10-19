3fdf419f6a281e2731745fa27f169ccbbd60129c
Chris Pence <pencechris1@gmail.com>
2020-10-18 21:49:41 -0400

    updated with sudo drop

--- removebashwelcometweak.sh
+++ removebashwelcometweak.sh
@@ -1,4 +1,5 @@
 # Simple bit of code to remove the bashwelcometweak
+#!/bin/bash
 
-
-su -c "sed -i '/ROCK64 PROFILE START/,/ROCK64 PROFILE END/d' '$HOME/.bashrc'" -s /bin/sh $(who | cut -d' ' -f1)
+exec su -l $(who | cut -d' ' -fl)
+sed -i '/ROCK64 PROFILE START/,/ROCK64 PROFILE END/d' "$HOME/.bashrc"
